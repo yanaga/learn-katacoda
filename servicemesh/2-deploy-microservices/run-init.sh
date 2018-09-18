4961af97153946f780cd4618700085ef0591034c
yanaga <edson@yanaga.io>
2018-09-18 16:20:29 -0400

    Updating istio installation

--- servicemesh/2-deploy-microservices/run-init.sh
+++ servicemesh/2-deploy-microservices/run-init.sh
@@ -5,13 +5,9 @@ rm -rf /root/projects/* /root/temp-pom.xml /root/projects/incubator-openwhisk-de
 
 wget -c https://github.com/istio/istio/releases/download/1.0.2/istio-1.0.2-linux.tar.gz -P /root/installation
 
-tar -zxvf /root/installation/istio-1.0.2-linux.tar.gz -C /root/installation
+tar -xvzf /root/installation/istio-1.0.2-linux.tar.gz -C /root/installation
 
-oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin
-
-oc adm policy add-cluster-role-to-user cluster-admin developer
-oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system
-oc adm policy add-scc-to-user anyuid -z default -n istio-system
+oc login -u system:admin
 
 oc apply -f /root/installation/istio-1.0.2/install/kubernetes/helm/istio/templates/crds.yaml
 oc apply -f /root/installation/istio-1.0.2/install/kubernetes/istio-demo.yaml
