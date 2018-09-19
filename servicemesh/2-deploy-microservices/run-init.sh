5b2fc47b9570530277514eba8912b751559372a5
yanaga <edson@yanaga.io>
2018-09-19 18:32:17 -0400

    Adding hack back to run-init.sh

--- servicemesh/2-deploy-microservices/run-init.sh
+++ servicemesh/2-deploy-microservices/run-init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+hostname -I | awk '{print $1 " master"}' | tee -a /etc/hosts
+setenforce 0
+
 until (oc status &> /dev/null); do sleep 1; done
 
 git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git fetch
