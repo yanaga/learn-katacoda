86db98c0e6714e5535e3f24da93a09644ba1bae6
yanaga <edson@yanaga.io>
2018-09-19 18:36:21 -0400

    Tweaking again set-env.sh

--- servicemesh/2-deploy-microservices/set-env.sh
+++ servicemesh/2-deploy-microservices/set-env.sh
@@ -1,4 +1,4 @@
 /usr/local/bin/launch.sh
 until $(oc get project istio-system &> /dev/null); do sleep 1; done
 export PATH=$PATH:/root/installation/istio-1.0.2/bin/
-cd ~/projects
+mkdir -p ~/projects && cd ~/projects
