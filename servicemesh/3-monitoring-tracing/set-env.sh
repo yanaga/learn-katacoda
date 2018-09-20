f17f64ef10360dced230bc53ff344b4ada4b3069
yanaga <edson@yanaga.io>
2018-09-20 11:56:47 -0400

    Waiting for recommendation to be ready

--- servicemesh/3-monitoring-tracing/set-env.sh
+++ servicemesh/3-monitoring-tracing/set-env.sh
@@ -1,3 +1,5 @@
 /usr/local/bin/launch.sh
 until $(oc get project istio-system &> /dev/null); do sleep 1; done
+until $(oc get pods -n tutorial | grep recommendation); do sleep 1; done
 mkdir -p ~/projects && cd ~/projects
+export PATH=$PATH:/root/installation/istio-1.0.2/bin
